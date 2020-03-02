ad3c379b3cd311a590897274f10664a7cce55c0f
Gonzalo Pincheira <g.pincheira.a@gmail.com>
2020-03-02 15:04:29 -0300

    ci(fix-now-version): fix now version on json files for deployment

--- .now/now.feature.sh
+++ .now/now.feature.sh
@@ -6,8 +6,7 @@ CLEAN_BRANCH_NAME=${CIRCLE_BRANCH//\//-};
 JSON=$(cat <<-EOF
 {
     "name": "$CIRCLE_PROJECT_REPONAME-$CLEAN_BRANCH_NAME",
-    "type": "npm",
-    "forwardNpm": true,
+    "version": 2,
     "alias": [
         "$CLEAN_BRANCH_NAME-circleci-now.jsconfcl.now.sh"
     ]
