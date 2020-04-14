4a6d8145dc7c880a943949cc46ae25847ac86e3a
fforres <ftorres@brex.com>
2020-04-14 12:29:42 -0700

    fix mobile and tweals

--- components/SubscribeForm/index.js
+++ components/SubscribeForm/index.js
@@ -2,23 +2,41 @@ import React from "react";
 import style from "./style.module.scss";
 
 function SubscribeForm() {
+  const onFormClick = React.useCallback((e) => {
+    e.preventDefault();
+  }, []);
   return (
     <div className={style.subscribeForm}>
       <p>
-        Entérate de todas nuestras novedades y avances!
+        Entérate de las novedades y avances!
         <small>(Descuida, detestamos el spam tanto como tú)</small>
       </p>
-      <form className={style.input}>
+      <form className={style.input} onClick={onFormClick}>
         <input
-          type="text"
+          autoFocus
+          type="email"
           placeholder="Email"
           name="email-input"
           input-mode="email"
           id="email"
         />
         <button type="submit" aria-label="suscribe">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-            <path d="M0 3v18h24v-18h-24zm6.623 7.929l-4.623 5.712v-9.458l4.623 3.746zm-4.141-5.929h19.035l-9.517 7.713-9.518-7.713zm5.694 7.188l3.824 3.099 3.83-3.104 5.612 6.817h-18.779l5.513-6.812zm9.208-1.264l4.616-3.741v9.348l-4.616-5.607z" />
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+            xmlSpace="preserve"
+          >
+            <g id="mail_1_">
+              <g>
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M0,4.01v11.91l6.27-6.27L0,4.01z M18.91,2.98H1.09L10,10.97L18.91,2.98z
+            M13.73,9.64L20,15.92V4.01L13.73,9.64z M10.5,12.54L10.5,12.54c-0.13,0.12-0.31,0.19-0.5,0.19s-0.37-0.07-0.5-0.19l0,0
+           l-2.11-1.89l-6.33,6.33h17.88l-6.33-6.33L10.5,12.54z"
+                />
+              </g>
+            </g>
           </svg>
         </button>
       </form>
