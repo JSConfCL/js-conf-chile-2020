a8230342c3bacd81ed3497f0faa84a5e602f8c7c
fforres <ftorres@brex.com>
2020-04-14 10:17:29 -0700

    Does not like URL()

--- next.config.js
+++ next.config.js
@@ -15,16 +15,6 @@ const appConfig = {
     config.resolve.alias["@components"] = path.join(__dirname, "components");
     config.resolve.alias["@helpers"] = path.join(__dirname, "helpers");
     config.resolve.alias["@styles"] = path.join(__dirname, "styles");
-    config.module.rules.push({
-      test: /\.(eot|woff|woff2|ttf)$/,
-      use: {
-        loader: "url-loader",
-        options: {
-          limit: 100000,
-          name: "[name].[ext]",
-        },
-      },
-    });
     return config;
   },
 };
